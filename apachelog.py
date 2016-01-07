108dec83614149602c69afa3d7a3a4090bc9aec8
nickriethoven <nickriethoven@hotmail.com>
2016-01-07 18:15:17 +0100

    Date Format gewijzigd van 30/Nov/2014 -> 30/11/2014

--- apachelog.py
+++ apachelog.py
@@ -13,6 +13,33 @@ def format_element(w):
     # [30/Nov/2014:06:25:46 +0100] -> 30/Nov/2014,06:25:46
     w[3] = w[3].replace('[', "", 1)
     w[3] = w[3].replace(':', ",", 1)
+    # Date van 30/Nov/2014 -> 30/11/2014
+    if w[3].__contains__("Jan"):
+        w[3] = w[3].replace("Jan", "01", 1)
+    elif w[3].__contains__("Feb"):
+        w[3] = w[3].replace("Feb", "02", 1)
+    elif w[3].__contains__("Mar"):
+        w[3] = w[3].replace("Mar", "03", 1)
+    elif w[3].__contains__("Apr"):
+        w[3] = w[3].replace("Apr", "04", 1)
+    elif w[3].__contains__("May"):
+        w[3] = w[3].replace("May", "04", 1)
+    elif w[3].__contains__("Jun"):
+        w[3] = w[3].replace("Jun", "05", 1)
+    elif w[3].__contains__("Jun"):
+        w[3] = w[3].replace("Jun", "06", 1)
+    elif w[3].__contains__("Jul"):
+        w[3] = w[3].replace("Jul", "07", 1)
+    elif w[3].__contains__("Aug"):
+        w[3] = w[3].replace("Aug", "08", 1)
+    elif w[3].__contains__("Sep"):
+        w[3] = w[3].replace("Sep", "09", 1)
+    elif w[3].__contains__("Oct"):
+        w[3] = w[3].replace("Oct", "10", 1)
+    elif w[3].__contains__("Nov"):
+        w[3] = w[3].replace("Nov", "11", 1)
+    elif w[3].__contains__("Dec"):
+        w[3] = w[3].replace("Dec", "12", 1)
     w[4] = w[4].replace(']', "", 1)
 
     # GET /whole_genome/view/F2 naar -> GET,whole_genome view F2
@@ -50,13 +77,15 @@ def write_format(w, outfile):
         # dan begint de loop waar de standaard opmaak eindigt, net zo lang als de lijst lang is.
         # dit heeft geen invloed op de lengte van 10 blokken
         for x in w[12:]:
+            x = x.replace('(', '')
+            x = x.replace(')', '')
             x = x.replace(',', '')
             outfile.write(x + " ")
         outfile.write("\n")
 
         # test test
 def directory_check():
-    list1 = os.listdir(path='C:\Users\Nick\Dropbox\nick\School\Scripts Log files\Script\Dirty\Apache Log')
+    list1 = os.listdir(path='C:/Users/Nick/Dropbox/nick/School/Scripts Log files/Script')
     list2 = []
     for x in list1:
         if x.__contains__("apache_access_log") and not x.__contains__("_clean"):
@@ -69,13 +98,16 @@ def process_files(list):
     for x in list:
         f = open(x, 'r')
         outfile = open(f.name + "_clean.txt", 'w')
+        outfile.write("IP,Date,Time,Timezone,HTTP Request,Doel Locatie,Protocol,Response,Package size,Browser,Source")
+        outfile.write("\n")
+        print("Opschonen Apache Log File: " + str(y))
+        y += 1
+
         for line in f:
             # woorden opsplitsen
             words = line.split()
             # wegschrijven van de woorden
-            print("Opschonen Apache Log File: " + str(y))
             write_format(words, outfile)
-            y += 1
 
 
 process_files(directory_check())
